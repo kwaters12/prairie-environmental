4795645f43365151a6160d10108c84fcc822887d
Kelly Waters <kellywaters@gmail.com>
2018-05-20 19:27:34 -0700

    Setting up redux

--- api/app/models/weed_report.rb
+++ api/app/models/weed_report.rb
@@ -1,4 +1,6 @@
 class WeedReport < ApplicationRecord
   belongs_to :client
   belongs_to :user
+  geocoded_by :address   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
 end
